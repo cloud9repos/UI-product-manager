36aa6319044dbbebc0fa79e42d12e72ecbcacc19
Abhishek Jaiswal <cloud9repos@gmail.com>
2016-11-16 20:16:42 +0000

    main layout completed

---
+++ app/addProducts/addProductsCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+    "use strict"
+    
+    angular
+        .module("productManagement")
+        .controller("AddProductsCtrl", 
+                    
+                        AddProductsCtrl);
+                    
+    function AddProductsCtrl() {
+        
+    }
+}())
--- app/app.js
+++ app/app.js
@@ -5,5 +5,50 @@
     "use strict"
     
     var app = angular.module("productManagement", 
-                            []);
+                            [
+                            "ui.router",
+                            "ui.mask",
+                            "ui.bootstrap"]);
+                            
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+                function($stateProvider, $urlRouterProvider) {
+                    
+                    console.log("==========test")
+                    
+                    //default url
+                    $urlRouterProvider.otherwise("/")
+                    
+                    
+                    $stateProvider
+                        //dashboard
+                        .state("dashboard", {
+                            url: "/",
+                            templateUrl: "app/dashboard/dashboardView.html",
+                            controller: "Dashboard as dashboardCtrlAls"
+                        })
+                        
+                        //add products
+                        .state("addProducts", {
+                            url: "/addProducts",
+                            templateUrl: "app/addProducts/addProductsView.html",
+                            controller: "AddProductsCtrl as addProductsCtrlAls"
+                        })
+                        
+                        //update products
+                        .state("updateProducts", {
+                            url: "/updateProducts",
+                            templateUrl: "app/updateProducts/updateProductsView.html",
+                            controller: "updateProducts as updateProductsCtrlAls"
+                        })
+                        
+                        //update products
+                        .state("faqs", {
+                            url: "/faqs",
+                            templateUrl: "app/updateProducts/faqsView.html",
+                            controller: "updateProducts as updateProductsCtrlAls"
+                        })
+                    
+                }])
+    
 }());
